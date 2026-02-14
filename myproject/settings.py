ab99dcd6483e065aa844c93f8844732271eeb6da
Sagar Raturi <sagarraturi85516@gmail.com>
2026-02-14 17:21:52 +0530

    Add requirements.txt file

--- myproject/settings.py
+++ myproject/settings.py
@@ -12,10 +12,12 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 import os
 from pathlib import Path
+from decouple import config
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+SECRET_KEY = config('SECRET_KEY')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
